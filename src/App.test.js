edd156529cbc7eeea4f7a52b8de183d1f2d70a83
Keeko <letkeeko@gmail.com>
2022-06-09 22:05:31 +1000

    improve testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import App from "./App";
+import "@testing-library/jest-dom/extend-expect";
 import { render, within, screen } from "@testing-library/react";
 import categoriesData from "./data/categories.json";
 import merchantsData from "./data/merchants.json";
@@ -37,39 +38,233 @@ describe("status", () => {
     const completeTransaction = transactionsData.find(
       ({ status }) => status === "complete"
     );
+
     renderApp({ transactions: [completeTransaction] });
 
     const statusCell = getStatusCellInRow(screen, 1, 0);
-    within(statusCell).getByText("complete");
+
+    const completeText = within(statusCell).getByText("complete");
+
+    expect(completeText).toBeInTheDocument();
   });
 
   it("should show transaction status when it is exported", () => {
     const completeTransaction = transactionsData.find(
       ({ status }) => status === "exported"
     );
+
     renderApp({ transactions: [completeTransaction] });
 
     const statusCell = getStatusCellInRow(screen, 1, 0);
-    within(statusCell).getByText("exported");
+
+    const exportedText = within(statusCell).getByText("exported");
+
+    expect(exportedText).toBeInTheDocument();
   });
 
   it("should show transaction status when it is incomplete", () => {
     const incompleteTransaction = transactionsData.find(
       ({ status }) => status === "incomplete"
     );
+
     renderApp({ transactions: [incompleteTransaction] });
 
     const statusCell = getStatusCellInRow(screen, 1, 0);
-    within(statusCell).getByText("incomplete");
+
+    const incompleteText = within(statusCell).getByText("incomplete");
+
+    expect(incompleteText).toBeInTheDocument();
   });
 });
 
 describe("date", () => {
   it("should show a formatted date", () => {
-    renderApp();
+    const completeTransaction = transactionsData.find(
+      ({ status }) => status === "complete"
+    );
+
+    renderApp({ transactions: [completeTransaction] });
 
     const statusCell = getStatusCellInRow(screen, 1, 1);
 
-    within(statusCell).getByText(/[0-9]{2}\s[A-Z]{3}\s[0-9]{4}/i);
+    const formattedDate = within(statusCell).getByText(
+      /[0-9]{2}\s[A-Z]{3}\s[0-9]{4}/i
+    );
+
+    expect(formattedDate).toBeInTheDocument();
+  });
+});
+
+describe("merchant name", () => {
+  it("should show existing merchant name", () => {
+    const completeTransaction = transactionsData.find(
+      ({ status }) => status === "complete"
+    );
+
+    const { name } = merchantsData.find(
+      ({ id }) => id === completeTransaction.merchant
+    );
+
+    renderApp({ transactions: [completeTransaction] });
+
+    const statusCell = getStatusCellInRow(screen, 1, 2);
+
+    const merchantName = within(statusCell).getByText(name);
+
+    expect(merchantName).toBeInTheDocument();
+  });
+});
+
+describe("team member", () => {
+  it("should show team member name", () => {
+    const completeTransaction = transactionsData.find(
+      ({ status }) => status === "complete"
+    );
+
+    renderApp({ transactions: [completeTransaction] });
+
+    const statusCell = getStatusCellInRow(screen, 1, 3);
+
+    const teamMemberName = within(statusCell).getByText(
+      completeTransaction.team_member
+    );
+
+    expect(teamMemberName).toBeInTheDocument();
+  });
+});
+
+describe("category", () => {
+  it("should show existing category", () => {
+    const completeTransaction = transactionsData.find(
+      ({ status }) => status === "complete"
+    );
+
+    const { name } = categoriesData.find(
+      ({ id }) => id === completeTransaction.category
+    );
+
+    renderApp({ transactions: [completeTransaction] });
+
+    const statusCell = getStatusCellInRow(screen, 1, 4);
+
+    const categoryName = within(statusCell).getByText(name);
+
+    expect(categoryName).toBeInTheDocument();
+  });
+});
+
+describe("amount", () => {
+  it("should show amount", () => {
+    const completeTransaction = transactionsData.find(
+      ({ status }) => status === "complete"
+    );
+
+    renderApp({ transactions: [completeTransaction] });
+
+    const statusCell = getStatusCellInRow(screen, 1, 5);
+
+    const amountWithDollarSign = within(statusCell).getByText(
+      "$" + completeTransaction.amount
+    );
+
+    expect(amountWithDollarSign).toBeInTheDocument();
+  });
+});
+
+describe("gst", () => {
+  it("should show gst", () => {
+    const completeTransaction = transactionsData.find(
+      ({ status }) => status === "complete"
+    );
+
+    renderApp({ transactions: [completeTransaction] });
+
+    const statusCell = getStatusCellInRow(screen, 1, 6);
+
+    const gstWithDollarSign = within(statusCell).getByText(
+      "$" + completeTransaction.gst
+    );
+
+    expect(gstWithDollarSign).toBeInTheDocument();
+  });
+});
+
+describe("budget", () => {
+  it("should show budget", () => {
+    const completeTransaction = transactionsData.find(
+      ({ status }) => status === "complete"
+    );
+
+    renderApp({ transactions: [completeTransaction] });
+
+    const statusCell = getStatusCellInRow(screen, 1, 7);
+
+    const budgetName = within(statusCell).getByText(completeTransaction.budget);
+
+    expect(budgetName).toBeInTheDocument();
+  });
+});
+
+describe("receipt", () => {
+  it("should be checked when it is true", () => {
+    const completeTransaction = transactionsData.find(
+      ({ receipt }) => receipt === true
+    );
+
+    renderApp({ transactions: [completeTransaction] });
+
+    const statusCell = getStatusCellInRow(screen, 1, 8);
+
+    const checkbox = within(statusCell).getByRole("checkbox");
+
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox).toBeChecked();
+  });
+
+  it("should be unchecked when it is false", () => {
+    const completeTransaction = transactionsData.find(
+      ({ receipt }) => receipt === false
+    );
+
+    renderApp({ transactions: [completeTransaction] });
+
+    const statusCell = getStatusCellInRow(screen, 1, 8);
+
+    const checkbox = within(statusCell).getByRole("checkbox");
+
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox).toHaveAttribute("data-ischecked", "false");
+  });
+});
+
+describe("billable", () => {
+  it("should be checked when it is true", () => {
+    const completeTransaction = transactionsData.find(
+      ({ billable }) => billable === true
+    );
+
+    renderApp({ transactions: [completeTransaction] });
+
+    const statusCell = getStatusCellInRow(screen, 1, 9);
+
+    const checkbox = within(statusCell).getByRole("checkbox");
+
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox).toBeChecked();
+  });
+
+  it("should be unchecked when it is false", () => {
+    const completeTransaction = transactionsData.find(
+      ({ receipt }) => receipt === false
+    );
+
+    renderApp({ transactions: [completeTransaction] });
+
+    const statusCell = getStatusCellInRow(screen, 1, 9);
+
+    const checkbox = within(statusCell).getByRole("checkbox");
+
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox).toHaveAttribute("data-ischecked", "false");
   });
 });
